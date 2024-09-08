be5ba9fb71fc44c11973cd681c892b7ad9acda3f
itsmealessandro <itsmealessandro@outlook.com>
2024-09-08 10:09:53 +0200

    [NEW] inserisci evento

--- REST_Aule_web/src/main/java/org/univaq/swa/template/resources/EventiRes.java
+++ REST_Aule_web/src/main/java/org/univaq/swa/template/resources/EventiRes.java
@@ -1,18 +1,28 @@
 package org.univaq.swa.template.resources;
 
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PATCH;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.sql.Time;
+import java.sql.Types;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
+
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
@@ -23,6 +33,12 @@ public class EventiRes {
 
   private static final String DS_NAME = "java:comp/env/jdbc/myaule";
   private static final String SQL_SELECT_EVENTO_BY_ID = "SELECT * FROM Evento WHERE ID=?";
+  private static final String I_EVENTO = "INSERT INTO Evento "
+      + "(IDMaster, nome, oraInizio, oraFine, descrizione, ricorrenza, Data,"
+      + " dataFineRicorrenza, tipologiaEvento, IDResponsabile, IDCorso, IDAula) "
+      + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+  ;
 
   private static Connection getPooledConnection() throws NamingException, SQLException {
     InitialContext ctx = new InitialContext();
@@ -73,8 +89,7 @@ public class EventiRes {
       HashMap<String, String> e_map;
       try (
           // Preparo la connessione al DB
-          Connection connection = getPooledConnection();
-          // Preparo la Query con i valori presi dalla GET
+          Connection connection = getPooledConnection(); // Preparo la Query con i valori presi dalla GET
           PreparedStatement ps = connection.prepareStatement(SQL_SELECT_EVENTO_BY_ID)) {
         ps.setInt(1, eID);
         try (ResultSet rs = ps.executeQuery()) {
@@ -97,31 +112,55 @@ public class EventiRes {
 
   }
 
-  /*
-   * @GET
-   * 
-   * @Path("{name: [a-zA-Z]+}")
-   * 
-   * @Produces(MediaType.APPLICATION_JSON)
-   * public Response getNamedItem(@PathParam("name") String name) throws
-   * RESTWebApplicationException {
-   * try {
-   * List<String> l = new ArrayList<>();
-   * try (Connection connection = getPooledConnection();
-   * PreparedStatement ps = connection.prepareStatement(SQL_SELECT_AUTHOR)) {
-   * ps.setString(1, "%" + name + "%");
-   * try (ResultSet rs = ps.executeQuery()) {
-   * while (rs.next()) {
-   * l.add(rs.getString("surname") + " " + rs.getString("name"));
-   * }
-   * }
-   * }
-   * return Response.ok(l).build();
-   * } catch (SQLException ex) {
-   * throw new RESTWebApplicationException("SQL: " + ex.getMessage());
-   * } catch (NamingException ex) {
-   * throw new RESTWebApplicationException("DB: " + ex.getMessage());
-   * }
-   * }
-   */
+  @POST
+  @Consumes("application/json")
+  @Path("postTest")
+  public Response testPost(
+      Map<String, Object> hm) {
+    return Response.ok(hm.get("a")).build();
+  }
+
+  @PUT
+  @Consumes("application/json")
+  @Path("nuovo")
+  public Response createEvento(Map<String, Object> e_map) {
+
+    try (
+        // Preparo la connessione al DB
+        Connection connection = getPooledConnection();
+        PreparedStatement ps = connection.prepareStatement(I_EVENTO)) {
+
+      // NOTE: Conversione a SQL
+      LocalTime oraInizioL = LocalTime.parse((String) e_map.get("oraInizio"));
+      LocalTime oraFineL = LocalTime.parse((String) e_map.get("oraFine"));
+      Time oraInizioSql = Time.valueOf(oraInizioL);
+      Time oraFineSql = Time.valueOf(oraFineL);
+      LocalDate localDate = LocalDate.parse((String) e_map.get("data"), DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+      Date dateSql = Date.valueOf(localDate);
+
+      ps.setNull(1, Types.NULL);
+      ps.setString(2, (String) e_map.get("nome"));
+      ps.setTime(3, oraInizioSql);
+      ps.setTime(4, oraFineSql);
+      ps.setString(5, (String) e_map.get("descrizione"));
+      ps.setString(6, "NESSUNA");// ricorrenza
+      ps.setDate(7, dateSql);
+      ps.setNull(8, Types.NULL);// DATA FINE RICORRENZA
+      ps.setString(9, (String) e_map.get("tipologia"));
+
+      ps.setInt(10, Integer.valueOf((String) e_map.get("responsabileID")));
+
+      ps.setInt(11, Integer.valueOf((String) e_map.get("corsoID")));
+
+      ps.setInt(12, Integer.valueOf((String) e_map.get("aulaID")));
+
+      ps.executeUpdate();
+      return Response.ok("Operazione Riuscita").build();
+    } catch (SQLException ex) {
+      throw new RESTWebApplicationException("SQL: " + ex.getMessage());
+    } catch (NamingException ex) {
+      throw new RESTWebApplicationException("DB: " + ex.getMessage());
+    }
+  }
+
 }
